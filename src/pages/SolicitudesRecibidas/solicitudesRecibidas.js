91db2a0e2a26ed83fd32a3bf8b2559270fdf3204
Andres Mejia <anmepe.sud@gmail.com>
2023-04-17 09:40:15 +0930

    UPDATE

--- src/pages/SolicitudesRecibidas/solicitudesRecibidas.js
+++ src/pages/SolicitudesRecibidas/solicitudesRecibidas.js
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from "react";
 import TableRow from "@mui/material/TableRow";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
+import { Button } from "@mui/material";
 
 
 const SolicitudesRecibidas = () => {
@@ -27,21 +28,27 @@ const SolicitudesRecibidas = () => {
     },[])
     console.log(data)
 
+    const edit =(()=>{
+
+    })
+    
     return(
     <Grid container justifyContent='center' width='100%' style={{ background: 'white'}}>
         <Grid item ml='20px'>
-            <Typography><b>Solicitudes</b></Typography>
+            <Typography variant="h5">Solicitudes</Typography>
         </Grid>
         <Grid item>
         <TableContainer >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell style={{color:'#0afdd7'}} align="left"><b>Name</b></TableCell>
-            <TableCell style={{color:'#0afdd7'}} align="left"><b>Last name</b></TableCell>
-            <TableCell style={{color:'#0afdd7'}} align="left"><b>Phone</b></TableCell>
-            <TableCell style={{color:'#0afdd7'}} align="left"><b>E-mail</b></TableCell>
-            <TableCell style={{color:'#0afdd7'}} align="left"><b>Message</b></TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Name</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Last name</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Phone</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">E-mail</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Messag</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Edit</TableCell>
+            <TableCell style={{color:'#0afdd7'}} align="left">Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -53,6 +60,8 @@ const SolicitudesRecibidas = () => {
                 <TableCell >{row.phone}</TableCell>
                 <TableCell >{row.email}</TableCell>
                 <TableCell >{row.message}</TableCell>
+                <TableCell ><Button>Edit</Button></TableCell>
+                <TableCell ><Button>Delete</Button></TableCell>
                </TableRow> 
             ))
           }
