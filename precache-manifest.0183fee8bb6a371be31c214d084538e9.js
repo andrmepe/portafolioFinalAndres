52f99bbb1647a895016848a22d8de403c51cc1fc
Andres Mejia <anmepe.sud@gmail.com>
2023-04-22 09:56:06 +0930

    Updates

---
+++ precache-manifest.0183fee8bb6a371be31c214d084538e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "45afae9baa7898241f10",
+    "url": "/static/css/main.bee8a5b5.chunk.css"
+  },
+  {
+    "revision": "45afae9baa7898241f10",
+    "url": "/static/js/main.45afae9b.chunk.js"
+  },
+  {
+    "revision": "ff2b081502721ca50082",
+    "url": "/static/js/1.ff2b0815.chunk.js"
+  },
+  {
+    "revision": "770608bdd992379a8016",
+    "url": "/static/js/2.770608bd.chunk.js"
+  },
+  {
+    "revision": "0396ddc0e961fb751ef5",
+    "url": "/static/js/runtime~main.0396ddc0.js"
+  },
+  {
+    "revision": "23686207ec585be94371aed26ecb26da",
+    "url": "/static/media/Contact.23686207.png"
+  },
+  {
+    "revision": "b4bc0c6991b02bb6981f2d5a32542d93",
+    "url": "/static/media/Profile.b4bc0c69.png"
+  },
+  {
+    "revision": "2dd95d4252ed20eccedab0157cc3e166",
+    "url": "/static/media/FondoNegro.2dd95d42.jpg"
+  },
+  {
+    "revision": "b3a6c2ee6fb715a819647266213445b1",
+    "url": "/static/media/Programmer2.b3a6c2ee.png"
+  },
+  {
+    "revision": "18257125c3ed7bcc36a1e855c54f4a4a",
+    "url": "/static/media/Network1.18257125.jpg"
+  },
+  {
+    "revision": "18257125c3ed7bcc36a1e855c54f4a4a",
+    "url": "/static/media/Network2.18257125.jpg"
+  },
+  {
+    "revision": "58dfbb6839eb7f835e5e7c1577fa493f",
+    "url": "/static/media/Web1.58dfbb68.png"
+  },
+  {
+    "revision": "070b5afe36ac844dda8e213fd3203bfa",
+    "url": "/static/media/Web2.070b5afe.png"
+  },
+  {
+    "revision": "ebb434ee095207e22916c2c3d0e89aaf",
+    "url": "/static/media/Web3.ebb434ee.png"
+  },
+  {
+    "revision": "91f60687e0998d96bc4074ef31c183cc",
+    "url": "/index.html"
+  }
+];
